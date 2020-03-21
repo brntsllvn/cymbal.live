e073d2e8710c92d162b2654cd341505f3dadd79b
Brent Sullivan <brntsllvn@gmail.com>
2020-03-21 16:01:21 -0700

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import ReactGA from 'react-ga';
 import ShowContainer from './components/ShowContainer'
 
 function App() {
+  const trackingId = "UA-161532154-1";
+  ReactGA.initialize(trackingId);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div>
       <div>
