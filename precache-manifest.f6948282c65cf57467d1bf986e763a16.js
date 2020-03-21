69f7f7939aba2b3fed934e6d972f9505e677c812
Brent Sullivan <brntsllvn@gmail.com>
2020-03-21 13:04:22 -0700

    Updates

---
+++ precache-manifest.f6948282c65cf57467d1bf986e763a16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf017ea8c39e941b98a241b36988381",
+    "url": "/cymbal.live/index.html"
+  },
+  {
+    "revision": "bc75ac696e2597b8f39d",
+    "url": "/cymbal.live/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d11f618bf06ddac092dd",
+    "url": "/cymbal.live/static/js/2.b370cc9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cymbal.live/static/js/2.b370cc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc75ac696e2597b8f39d",
+    "url": "/cymbal.live/static/js/main.04fb3d51.chunk.js"
+  },
+  {
+    "revision": "6def4bcc7ee24f65a4e6",
+    "url": "/cymbal.live/static/js/runtime-main.b4aad9f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cymbal.live/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cymbal.live/precache-manifest.f6948282c65cf57467d1bf986e763a16.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cymbal.live/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
